d492cfc930a9d6a9f7115b0470f636448020b31c
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-06-18 18:52:57 -0400

    no idea

--- js/index.js
+++ js/index.js
@@ -6,40 +6,94 @@ logoHeading.addEventListener('mouseover', function(event){
 })
 
 let workBtn = document.querySelectorAll('.btn')
-workBtn.forEach((element,i) => {
-    element[i].addEventListener('dblclick', function(event){
-        console.log(`Buttons`);
-})
-// workBtn[0].addEventListener('dblclick', function(event){
-//     console.log(`Buttons`);
-// })
-// workBtn[1].addEventListener('dblclick', function(event){
-//     console.log(`Buttons`);
-// })
-// workBtn[2].addEventListener('dblclick', function(event){
-//     console.log(`Buttons`);
-// })
+workBtn.forEach(function(item, idx){
+    item.addEventListener('toggle', function(){
+        console.log(`buttons`);
+        event.target.style.backgroundColor = 'pink';
+    });
+});
+function backgroundItem(e){
+    const item = document.querySelector()
+    item.toggleAttribute('hidden');
+}
 
 let selectedNavLinks = document.querySelectorAll('nav a');
-selectedNavLinks.forEach(function(links, i){
-
-})
+selectedNavLinks.forEach(function(item, idx){
+    item.addEventListener('click', function(){
+        console.log('nav links!');
+        event.target.style.backgroundColor = 'red';
+        event.preventDefault();
+    });
+});
 
 let headerImg = document.querySelector('header img');
 headerImg.addEventListener('drag', function(event){
     console.log(`Image selected`);
+    event.target.style.display = 'none';
 })
 
-let h2selection = document.getElementsByTagName('h2');
-h2selection[0].addEventListener('click', function(event){
-    console.log(`H2 selected`);
-})
-h2selection[1].addEventListener('click', function(event){
-    console.log(`H2 selected`);
-})
-h2selection[2].addEventListener('click', function(event){
-    console.log(`H2 selected`);
-})
-h2selection[3].addEventListener('click', function(event){
-    console.log(`H2 selected`);
-})
+let h2selection = document.querySelectorAll('h2');
+h2selection.forEach(function(item, idx){
+    item.addEventListener('mouseover', function(){
+        console.log(`H2!`);
+        event.target.style.color = 'red';
+    });
+
+});
+
+// h2selection[0].addEventListener('click', function(event){
+//     console.log(`H2 selected`);
+//     event.target.style.fontsize = '2rem';
+// })
+// h2selection[1].addEventListener('click', function(event){
+//     console.log(`H2 selected`);
+// })
+// h2selection[2].addEventListener('click', function(event){
+//     console.log(`H2 selected`);
+// })
+// h2selection[3].addEventListener('click', function(event){
+//     console.log(`H2 selected`);
+// })
+
+// const homeContainer = document.querySelector('.container home');
+// homeContainer.addEventListener('click', function(event){
+//     console.log(`scrolling in the ${event.target}`)
+// })
+// const container = document.querySelector('.container')[1]
+// container.addEventListener('click', event => {
+//   console.log('Clicked', event.target, event.screenX, event.screenY)
+// })
+
+
+window.addEventListener('load', (event) => {
+    console.log(`page is fully loaded`);
+});
+
+let textHighlight = document.querySelectorAll('p')
+textHighlight.forEach(function(item, idx){
+    item.addEventListener('copy', function(){
+        console.log(`text`);
+        event.target.style.fontSize = '3rem';
+    });
+
+});
+
+// let imageZoom = document.querySelector('.img-fluid')
+// imageZoom.addEventListener('click', function(event){
+//     console.log(`image!`)
+// })
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    el.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  const el = document.querySelector('.img-fluid');
+  el.onwheel = zoom;
